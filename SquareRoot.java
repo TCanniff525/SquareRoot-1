6f68dace8dd8af0e7f45c14082d5e70052fffec7
TCanniff525 <71709195+TCanniff525@users.noreply.github.com>
2020-10-08 13:43:14 -0400

    lol

--- SquareRoot.java
+++ SquareRoot.java
@@ -17,10 +17,10 @@ public class SquareRoot {
         double radicand = input.nextDouble();
 
         // Check that the input is not negative
-        // TODO: Change this part of the program to repeatedly error check
-        if (radicand < 0) {
+        while (radicand < 0) {
             System.err.println("Invalid input!");
-            System.exit(1);
+            System.err.print("Enter a number to square root: ");
+            radicand = input.nextDouble();
         }
 
         // Calculate the circumference
